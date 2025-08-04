eed8e7b37b733c7fd500f1dca1e1870376617ac0
Anatoliy <a.yanovich@jdplex.com>
2025-08-04 16:36:22 +0300

    finish commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
+//import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import "primeicons/primeicons.css"
@@ -17,7 +17,7 @@ const app = createApp(App)
 
 
 app.use(createPinia())
-app.use(router)
+//app.use(router)
 app.use(PrimeVue,{
     theme: {
         preset: Aura
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+/*import HomeView from '../views/HomeView.vue'
 
 
 const router = createRouter({
@@ -29,4 +29,4 @@ const router = createRouter({
   ],
 })
 
-export default router
+export default router*/
